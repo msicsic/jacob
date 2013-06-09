8101d06350ccea0d5f8bb9049664e7142cfdeb9c
Robert Gallas <gallas.robert@gmail.com>
2013-06-09 20:22:37 +0200

    Start of table create support

---
+++ sql/src/main/java/sk/jacob/sql/Metadata.java
@@ -0,0 +1,7 @@
+package sk.jacob.sql;
+
+public class Metadata {
+    public void add(Table table) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+}
---
+++ sql/src/main/java/sk/jacob/sql/Table.java
@@ -0,0 +1,8 @@
+package sk.jacob.sql;
+
+public class Table {
+    public Table(Metadata metadata) {
+        metadata.add(this);
+
+    }
+}
