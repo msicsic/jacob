fca1a6c31b8228561ec48c9b3c8164da7450a020
Robert Gallas <gallas.robert@gmail.com>
2013-06-12 23:44:15 +0200

    Refaktor sserializera

--- engine/src/main/java/sk/jacob/engine/handler/HandlerSerializer.java
+++ engine/src/main/java/sk/jacob/engine/handler/HandlerSerializer.java
@@ -10,75 +10,95 @@ import java.util.Map;
 import java.util.Set;
 
 public class HandlerSerializer {
+    private static final Set<Class> BOXED_TYPES = initBoxedTypes();
 
-    private static final Set<Class> PRIMITIVE_TYPES = new HashSet<Class>() {
-        {
-            add(byte.class);
+    private static Set<Class> initBoxedTypes(){
+        return new HashSet<Class>() {{
             add(Byte.class);
-            add(char.class);
             add(Character.class);
-            add(short.class);
             add(Short.class);
-            add(int.class);
             add(Integer.class);
-            add(long.class);
             add(Long.class);
-            add(float.class);
             add(Float.class);
-            add(double.class);
             add(Double.class);
-            add(boolean.class);
             add(Boolean.class);
-            add(String.class);
-        }
-    };
+            add(String.class);}};
+    }
 
     public static Map<String, Object> serialize(Method method) {
         Map<String, Object> handlerMap = new HashMap<String, Object>();
-
         Message messageAnnotation = method.getAnnotation(Message.class);
 
         handlerMap.put("type", messageAnnotation.type());
         handlerMap.put("version", messageAnnotation.version());
         handlerMap.put("resd", serializeClass(messageAnnotation.reqd()));
         handlerMap.put("reqd", serializeClass(messageAnnotation.resd()));
+
         return handlerMap;
     }
 
     private static Map<String, Object> serializeClass(Class resReqClass) {
         Map<String, Object> resMap = new HashMap<String, Object>();
 
+        // Handluje zdedene fieldy?
         for (Field field : resReqClass.getDeclaredFields()) {
             Class fieldClass = field.getType();
-            if (fieldClass.getModifiers() != 0 && (field.getModifiers() & Modifier.STATIC) == 0) {
-                Object fieldObj;
-                if (PRIMITIVE_TYPES.contains(fieldClass)) {
-                    HashMap<String, Object> map = new HashMap<String, Object>();
-                    map.put("type", fieldClass.getCanonicalName());
-                    map.put("required", field.getAnnotation(Required.class) != null);
-                    Length lengthAnnot = field.getAnnotation(Length.class);
-                    if (lengthAnnot != null) {
-                        map.put("minLength", lengthAnnot.min());
-                        map.put("maxLength", lengthAnnot.max());
-                    }
-                    fieldObj = map;
-                } else if (fieldClass.isArray()) {
-                    Set<Object> set = new HashSet<Object>();
-                    set.add(serializeClass(fieldClass.getComponentType()));
-                    fieldObj = set;
-                } else if (Iterable.class.isAssignableFrom(fieldClass)) {
-                    ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
-                    Set<Object> set = new HashSet<Object>();
-                    set.add(serializeClass((Class) parameterizedType.getActualTypeArguments()[0]));
-                    fieldObj = set;
-                } else if (Map.class.isAssignableFrom(fieldClass)) {
-                    fieldObj = new Object();
-                } else {
-                    fieldObj = serializeClass(fieldClass);
-                }
-                resMap.put(field.getName(), fieldObj);
+
+            if (   fieldClass.getModifiers() == 0
+                    || (field.getModifiers() & Modifier.STATIC) != 0)
+                continue;
+
+            Object fieldObj;
+            if (fieldClass.isPrimitive() || BOXED_TYPES.contains(fieldClass)) {
+                fieldObj = handlePrimitive(fieldClass, field);
+            } else if (fieldClass.isArray()) {
+                fieldObj = handleArray(fieldClass);
+            } else if (Iterable.class.isAssignableFrom(fieldClass)) {
+                fieldObj = handleIterable(field);
+            } else if (Map.class.isAssignableFrom(fieldClass)) {
+                fieldObj = new Object();
+            } else {
+                fieldObj = serializeClass(fieldClass);
             }
+            resMap.put(field.getName(), fieldObj);
         }
         return resMap;
+    }
+
+    private static Map<String, Object> handlePrimitive(Class fieldClass, Field field) {
+        // Prilis genericke
+        //
+        // String ma minLength, maxLength
+        // Cisla minValue, MaxValue
+        // Bolo by fajn mat aj regexp, kt by sa podmienene daval do mapy
+        return handleLengthAnnotation(field.getAnnotation(Length.class),
+                handleHeader(fieldClass, field, new HashMap<String, Object>()));
+    }
+
+    private static Map<String, Object> handleHeader(Class fieldClass, Field field, Map<String, Object> map) {
+        map.put("type", fieldClass.getSimpleName());
+        map.put("required", field.getAnnotation(Required.class) != null);
+        return map;
+    }
+
+    private static Map<String, Object> handleLengthAnnotation(Length lengthAnnot, Map<String, Object> map) {
+        if (lengthAnnot != null) {
+            map.put("minLength", lengthAnnot.min());
+            map.put("maxLength", lengthAnnot.max());
+        }
+        return map;
+    }
+
+    private static Set<Object> handleArray(Class fieldClass) {
+        Set<Object> set = new HashSet<Object>();
+        set.add(serializeClass(fieldClass.getComponentType()));
+        return set;
+    }
+
+    private static Set<Object> handleIterable(Field field) {
+        Set<Object> set = new HashSet<Object>();
+        ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
+        set.add(serializeClass((Class) parameterizedType.getActualTypeArguments()[0]));
+        return set;
     }
 }
